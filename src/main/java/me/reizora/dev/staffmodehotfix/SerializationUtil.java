bf414e9265febebdd8c5f0dc348af151661ac2f5
Reizora <lauglaugjefter@gmail.com>
2024-05-15 21:32:30 +0800

    Hotfix plugin working

--- src/main/java/me/reizora/dev/staffmodehotfix/SerializationUtil.java
+++ src/main/java/me/reizora/dev/staffmodehotfix/SerializationUtil.java
@@ -1,14 +1,17 @@
 package me.reizora.dev.staffmodehotfix;
 
 import org.bukkit.inventory.ItemStack;
+
 import java.io.*;
 import java.util.Base64;
+import java.util.Map;
 
 public class SerializationUtil {
+
     public static String itemStackArrayToBase64(ItemStack[] items) throws IOException {
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
-            objectOutputStream.writeObject(items);
+            objectOutputStream.writeObject(serializeItemStackArray(items));
         }
         return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
     }
@@ -17,7 +20,48 @@ public class SerializationUtil {
         byte[] bytes = Base64.getDecoder().decode(data);
         ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
         try (ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream)) {
-            return (ItemStack[]) objectInputStream.readObject();
+            return deserializeItemStackArray((String[]) objectInputStream.readObject());
+        }
+    }
+
+    private static String[] serializeItemStackArray(ItemStack[] items) {
+        String[] serializedItems = new String[items.length];
+        for (int i = 0; i < items.length; i++) {
+            serializedItems[i] = items[i] == null ? "null" : itemStackToBase64(items[i]);
+        }
+        return serializedItems;
+    }
+
+    private static ItemStack[] deserializeItemStackArray(String[] serializedItems) {
+        ItemStack[] items = new ItemStack[serializedItems.length];
+        for (int i = 0; i < serializedItems.length; i++) {
+            items[i] = "null".equals(serializedItems[i]) ? null : itemStackFromBase64(serializedItems[i]);
+        }
+        return items;
+    }
+
+    public static String itemStackToBase64(ItemStack item) {
+        if (item == null) return "null";
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        try (ObjectOutputStream dataOutput = new ObjectOutputStream(outputStream)) {
+            Map<String, Object> serialized = item.serialize();
+            dataOutput.writeObject(serialized);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return Base64.getEncoder().encodeToString(outputStream.toByteArray());
+    }
+
+    public static ItemStack itemStackFromBase64(String data) {
+        if (data.equals("null")) return null;
+        byte[] bytes = Base64.getDecoder().decode(data);
+        ByteArrayInputStream inputStream = new ByteArrayInputStream(bytes);
+        try (ObjectInputStream dataInput = new ObjectInputStream(inputStream)) {
+            Map<String, Object> serialized = (Map<String, Object>) dataInput.readObject();
+            return ItemStack.deserialize(serialized);
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+            return null;
         }
     }
 }
--- src/main/java/me/reizora/dev/staffmodehotfix/StaffModeHotFix.java
+++ src/main/java/me/reizora/dev/staffmodehotfix/StaffModeHotFix.java
@@ -1,14 +1,12 @@
 package me.reizora.dev.staffmodehotfix;
 
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -17,45 +15,64 @@ import java.io.*;
 import java.util.HashMap;
 import java.util.UUID;
 
-public final class StaffModeHotFix extends JavaPlugin implements Listener, CommandExecutor {
-    private HashMap<UUID, String> savedInventoryData = new HashMap<>();
+public final class StaffModeHotFix extends JavaPlugin implements Listener{
+    private HashMap<UUID, String> savedInventoryData = new HashMap<>(); //Hashmap to store player inventory data in case of a server crash. Hashmap data will be saved into a .ser file as local backup.
 
     @Override
     public void onEnable() {
-        File dataFolder = getDataFolder();
-        if (!dataFolder.exists()) {
-            dataFolder.mkdirs();
+        try {
+            File dataFolder = new File(getDataFolder(), "README.yml"); //Create plugin directory and a README.yml file.
+            if (!dataFolder.exists()) {
+                dataFolder.getParentFile().mkdirs();
+                dataFolder.createNewFile();
+            }
+            FileWriter writeReadMe = new FileWriter(dataFolder);
+            writeReadMe.write("DO NOT DELETE THIS DIRECTORY AND THE STAFFMODEHOTFIX JAR!\n\n");
+            writeReadMe.write("THIS PLUGIN IS CRUCIAL FOR THE STAFFMODE PLUGIN.\n");
+            writeReadMe.write("DELETING THIS PLUGIN WILL RESULT TO ITEM LOSSES WHEN SERVER CRASHES WHILE IN STAFFMODE!\n\n\n\n\n\n\n");
+            writeReadMe.write("Discord: reizora_");
+            writeReadMe.close();
+            System.out.println("README.txt created!");
+        } catch (IOException e) {
+            System.out.println("File creation error!");
+            e.printStackTrace();
         }
-
         getServer().getPluginManager().registerEvents(this, this);
-        this.getCommand("staffmode").setExecutor(this);
-        loadHashMapFromFile();
-
-        System.out.println("HOTFIX ENABLED!");
-        System.out.println("THIS PLUGIN WORKS IN CONJUNCTION WITH THE STAFFMODE PLUGIN, DO NOT DELETE!");
+        loadHashMapFromFile(); //Load HashMap data from the .ser file into the plugin.
+        System.out.println("STAFFMODE HOTFIX ENABLED!");
     }
 
     @Override
     public void onDisable() {
-        saveHashMapToFile();
+        //Not used.
     }
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player player) {
-            if (command.getName().equalsIgnoreCase("staffmode")) {
+    @EventHandler
+    public void onPlayerCommand(PlayerCommandPreprocessEvent event){ //listens for when a player with the "staffmode.staff" perms does a /staff or /staffmode command.
+        Player player = event.getPlayer();
+        String command = event.getMessage();
+        if (!player.hasPermission("staffmode.staff")) {
+            player.sendMessage(ChatColor.RED+ "You are not a staff!");
+            System.out.println("Player " +player.getName()+ " is not a staff!");
+        }
+        else {
+            if((command.equalsIgnoreCase("/staffmode") || command.equalsIgnoreCase("/staff")) && (player.getGameMode() != GameMode.CREATIVE)){
                 try {
                     ItemStack[] inventoryData = player.getInventory().getContents();
                     String serializedData = SerializationUtil.itemStackArrayToBase64(inventoryData);
                     savedInventoryData.put(player.getUniqueId(), serializedData);
-                    System.out.println("Inventory data saved!");
+                    saveHashMapToFile();
+
+                    System.out.println("Player " +player.getName()+ " has entered staffmode!");
+                    System.out.println("Player " +player.getName()+"'s items before entering staffmode has been saved!");
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                return true;
+            }
+            else{
+                System.out.println("Player " +player.getName()+ "is leaving staffmode.");
             }
         }
-        return false;
     }
 
     @EventHandler
@@ -63,14 +80,8 @@ public final class StaffModeHotFix extends JavaPlugin implements Listener, Comma
         Player player = event.getPlayer();
         if (player.hasPermission("staffmode.staff") && player.getGameMode() == GameMode.CREATIVE) {
             player.setGameMode(GameMode.SURVIVAL);
-            saveHashMapToFile();
             restoreItems(player);
         }
-    }
-
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        //saveHashMapToFile();
     }
 
     public void restoreItems(Player player) {
@@ -90,17 +101,13 @@ public final class StaffModeHotFix extends JavaPlugin implements Listener, Comma
     private void saveHashMapToFile() {
         try {
             File file = new File(getDataFolder(), "inventoryData.ser");
-            if (!file.exists()) {
-                file.getParentFile().mkdirs();
-                file.createNewFile();
-            }
             try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file))) {
                 oos.writeObject(savedInventoryData);
-                System.out.println("HashMap saved to file.");
+                System.out.println("HashMap data saved to file.");
             }
         } catch (IOException e) {
             e.printStackTrace();
-            System.err.println("Failed to save HashMap to file.");
+            System.err.println("Failed to save HashMap data to file.");
         }
     }
 
